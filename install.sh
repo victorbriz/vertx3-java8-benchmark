8fd4bbebbbc52825757f3463e253d3c1dc20f86b
victorbriz <github@briz.ws>
2015-04-30 08:36:41 -0700

    Updating install script with freemarker jar

--- install.sh
+++ install.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-fw_depends java8
+fw_depends java7
 
-RETCODE=$(fw_exists ${IROOT}/vert.x-3.0.0-milestone4.installed)
+RETCODE=$(fw_exists ${IROOT}/vert.x-2.1.5.installed)
 [ ! "$RETCODE" == 0 ] || { return 0; }
 
-fw_get http://dl.bintray.com/vertx/downloads/vert.x-3.0.0-milestone4.tar.gz?direct=true -O vert.x-3.0.0-milestone4.tar.gz
-fw_untar vert.x-3.0.0-milestone4.tar.gz
-
-touch ${IROOT}/vert.x-3.0.0-milestone4.installed
+fw_get http://dl.bintray.com/vertx/downloads/vert.x-2.1.5.tar.gz?direct=true -O vert.x-2.1.5.tar.gz
+fw_untar vert.x-2.1.5.tar.gz
+wget http://central.maven.org/maven2/org/freemarker/freemarker/2.3.22/freemarker-2.3.22.jar -O ${IROOT}/vert.x-2.1.5/lib/freemarker-2.3.22.jar
+touch ${IROOT}/vert.x-2.1.5.installed
