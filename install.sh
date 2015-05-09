fb2ee1a04889baa877872d8b7264fc9b82db2b63
victorbriz <github@briz.ws>
2015-05-09 14:54:18 -0700

    Updating install script with milestone5

--- install.sh
+++ install.sh
@@ -5,7 +5,7 @@ fw_depends java8
 RETCODE=$(fw_exists ${IROOT}/vert.x-3.0.0.installed)
 [ ! "$RETCODE" == 0 ] || { return 0; }
 
-fw_get http://dl.bintray.com/vertx/downloads/vert.x-3.0.0-milestone4.tar.gz?direct=true -O vert.x-3.0.0.tar.gz
+fw_get http://dl.bintray.com/vertx/downloads/vert.x-3.0.0-milestone5.tar.gz?direct=true -O vert.x-3.0.0.tar.gz
 fw_untar vert.x-3.0.0.tar.gz
 wget http://central.maven.org/maven2/org/freemarker/freemarker/2.3.22/freemarker-2.3.22.jar -O ${IROOT}/vert.x-3.0.0/lib/freemarker-2.3.22.jar
 touch ${IROOT}/vert.x-3.0.0.installed
