542405d9b165895f91d1cdcd68d034dd6a266cc5
victorbriz <github@briz.ws>
2015-05-10 12:50:45 -0700

    Updating setup for milestone5

--- setup.sh
+++ setup.sh
@@ -4,4 +4,4 @@ source $IROOT/java8.installed
 
 sed -i 's|host: \x27.*\x27|host: \x27'"${DBHOST}"'\x27|g' app.js
 
-${IROOT}/vert.x-3.0.0-milestone4/bin/vertx run app.js &
+${IROOT}/vert.x-3.0.0-milestone5/bin/vertx run app.js &
